3b9e33cffd6f419a03abfba4753c810ef53f4ce8
ytnew3774-star <ytnew3774@gmail.com>
2026-04-29 17:40:19 +0530

    fix reconnect bug and cancel search

--- server.js
+++ server.js
@@ -17,31 +17,40 @@ let onlineCount = 0;
 io.on('connection', (socket) => {
   onlineCount++;
   io.emit('onlineCount', onlineCount);
-  console.log('User connected:', socket.id, '| Online:', onlineCount);
 
   socket.on('findStranger', ({ myGender, targetGender }) => {
+    // Pehle queue se hatao agar pehle se hai
+    removeFromQueue(socket);
+    
     socket.myGender = myGender;
     socket.targetGender = targetGender;
+    socket.isSearching = true;
 
     let matched = null;
     if (targetGender === 'male') {
-      const idx = waitingMale.findIndex(s => s.targetGender === myGender);
+      const idx = waitingMale.findIndex(s => s.targetGender === myGender && s.isSearching);
       if (idx !== -1) matched = waitingMale.splice(idx, 1)[0];
     } else {
-      const idx = waitingFemale.findIndex(s => s.targetGender === myGender);
+      const idx = waitingFemale.findIndex(s => s.targetGender === myGender && s.isSearching);
       if (idx !== -1) matched = waitingFemale.splice(idx, 1)[0];
     }
 
     if (matched) {
       pairs[socket.id] = matched.id;
       pairs[matched.id] = socket.id;
+      socket.isSearching = false;
+      matched.isSearching = false;
       socket.emit('matched', { peerGender: targetGender });
       matched.emit('matched', { peerGender: myGender });
     } else {
       if (myGender === 'male') waitingMale.push(socket);
       else waitingFemale.push(socket);
-      socket.emit('waiting');
     }
+  });
+
+  socket.on('cancelSearch', () => {
+    socket.isSearching = false;
+    removeFromQueue(socket);
   });
 
   socket.on('message', (msg) => {
@@ -50,29 +59,37 @@ io.on('connection', (socket) => {
   });
 
   socket.on('next', () => {
-    disconnectPeer(socket);
-    socket.emit('findNext');
+    socket.isSearching = false;
+    const peerId = pairs[socket.id];
+    if (peerId) {
+      const peer = io.sockets.sockets.get(peerId);
+      if (peer) {
+        peer.emit('peerLeft');
+        delete pairs[peerId];
+      }
+      delete pairs[socket.id];
+    }
   });
 
   socket.on('disconnect', () => {
     onlineCount--;
     io.emit('onlineCount', onlineCount);
-    disconnectPeer(socket);
-    const mi = waitingMale.indexOf(socket);
-    if (mi !== -1) waitingMale.splice(mi, 1);
-    const fi = waitingFemale.indexOf(socket);
-    if (fi !== -1) waitingFemale.splice(fi, 1);
-    console.log('User disconnected | Online:', onlineCount);
+    socket.isSearching = false;
+    removeFromQueue(socket);
+    const peerId = pairs[socket.id];
+    if (peerId) {
+      io.to(peerId).emit('peerLeft');
+      delete pairs[peerId];
+      delete pairs[socket.id];
+    }
   });
 });
 
-function disconnectPeer(socket) {
-  const peerId = pairs[socket.id];
-  if (peerId) {
-    io.to(peerId).emit('peerLeft');
-    delete pairs[peerId];
-    delete pairs[socket.id];
-  }
+function removeFromQueue(socket) {
+  const mi = waitingMale.indexOf(socket);
+  if (mi !== -1) waitingMale.splice(mi, 1);
+  const fi = waitingFemale.indexOf(socket);
+  if (fi !== -1) waitingFemale.splice(fi, 1);
 }
 
 const PORT = process.env.PORT || 3000;
